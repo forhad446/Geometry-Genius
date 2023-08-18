00923695015e12d8e04aa42e31f214b4a8b3b22d
forhad446 <forhadkkhan@gmail.com>
2023-08-18 10:35:49 +0600

    all done this project

--- js/utilities.js
+++ js/utilities.js
@@ -11,12 +11,26 @@ function calculationEntry(areaType,result){
     const calculation_entry = document.getElementById('calculation_entry');
     const count = calculation_entry.childElementCount;
     const p = document.createElement('p');
+    const deleteHistory = document.createElement('p');
     p.innerHTML = `${count + 1}. ${areaType} ${result.toFixed(2)} cm<sup>2</sup> <button class="bg-green-600 px-2 text-white rounded-md">convert</button>`;
     p.classList.add('mt-2')
     if(count < 15){
         calculation_entry.appendChild(p);
     }else{
-        alert('please clear the all history');
-        return;
+        if(count == 16){
+            alert('please clear the all history');
+            return;
+        }
+        deleteHistory.innerText = 'Delete all history';
+        deleteHistory.setAttribute('onclick','deleteHistory()')
+        deleteHistory.setAttribute('id','deleteHistory')
+        deleteHistory.classList.add('bg-red-600','px-2','text-white','rounded-md','mt-2','cursor-pointer')
+        calculation_entry.appendChild(deleteHistory);
+    }
+}
+function deleteHistory(){
+    const parent = document.getElementById('deleteHistory').parentNode;
+    while(parent.hasChildNodes()){
+        parent.removeChild(parent.firstChild)
     }
 }
